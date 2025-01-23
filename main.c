e7d60c8e7d99e52e140a9921c781e47e0b00a603
Ravel Souza <ravelsouza.eletromecanica@gmail.com>
2025-01-23 12:51:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -58,12 +58,47 @@ static inline void acender_leds(PIO pio, uint sm, uint len, int leds[], int row,
     }
 }
 
+static inline void teste_esteira(PIO pio, uint sm, uint len, int leds[], int row, uint32_t* cor) {
+    // Apaga todos os LEDs
+    int cor_led = 0;
+    for (uint i = 0; i < len; ++i) {
+        bool led_aceso = false;
+        // Verifica se o LED atual deve ser aceso (comparando com a linha do vetor de LEDs)
+        for (int j = 0; j <= row; ++j) {
+            if (leds[j] == i) {
+                put_pixel(pio, sm, cor[cor_led]); // Acende o LED na cor especificada
+                cor_led += 1;
+                led_aceso = true;
+                break;
+            }
+        }
+        if (!led_aceso) {
+            put_pixel(pio, sm, urgb_u32(0, 0, 0)); // Apaga os LEDs não escolhidos
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
-    
     gpio_init(6);
     gpio_set_dir(6, GPIO_IN);
     gpio_pull_up(6);
+    gpio_init(4);
+    gpio_set_dir(4, GPIO_IN);
+    gpio_init(8);
+    gpio_set_dir(8, GPIO_IN);
+    gpio_init(9);
+    gpio_set_dir(9, GPIO_IN);
+    gpio_init(16);
+    gpio_set_dir(16, GPIO_IN);
+    gpio_init(17);
+    gpio_set_dir(17, GPIO_IN);
+    gpio_init(18);
+    gpio_set_dir(18, GPIO_IN);
+    gpio_init(19);
+    gpio_set_dir(19, GPIO_IN);
+    gpio_init(20);
+    gpio_set_dir(20, GPIO_IN);
 
     PIO pio;
     uint sm;
@@ -130,12 +165,84 @@ int main() {
         }
         acender_leds(pio, sm, 25, inicial[7], 7, cores[3]);
         sleep_ms(300);
-        for(int i = 0; i < 3; i++)
+        for(int i = 0; i < 2; i++)
         {
         acender_leds(pio, sm, 25, inicial[7], 7, urgb_u32(0,0,0));
         sleep_ms(300);
         acender_leds(pio, sm, 25, inicial[7], 7, cores[3]);
         sleep_ms(300);
+        }
+        uint32_t simulacao[8] = {};
+        
+        while(1)
+        {
+        if(gpio_get(4))
+        {
+            simulacao[0] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[0] = urgb_u32(50,0,0);
+        }
+        if(gpio_get(8))
+        {
+            simulacao[1] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[1] = urgb_u32(50,0,0);
+        }
+        if(gpio_get(9))
+        {
+             simulacao[2] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[2] = urgb_u32(50,0,0);
+        }
+        if(gpio_get(16))
+        {    
+            simulacao[3] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[3] = urgb_u32(50,0,0);
+        }
+        if(gpio_get(17))
+        {
+              simulacao[4] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[4] = urgb_u32(50,0,0);
+        }
+        if(gpio_get(18))
+        {
+             simulacao[5] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[6] = urgb_u32(50,0,0);
+        }
+        if(gpio_get(19))
+        {
+             simulacao[6] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[6] = urgb_u32(50,0,0);
+        }
+
+        if(gpio_get(20))
+        {
+              simulacao[7] = urgb_u32(0,50,0);
+        }
+        else
+        {
+            simulacao[7] = urgb_u32(50,0,0);
+        }
+        teste_esteira(pio, sm, 25, inicial[7], 7, simulacao);
+        sleep_ms(1000);
         }
         }
     }
